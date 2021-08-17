25ea8aa94d6384fbae523f876ba8c875beaffbd1
nanci <yunlong.yang@casstime.com>
2021-08-17 23:29:02 +0800

    %declation%

--- commit.sh
+++ commit.sh
@@ -14,8 +14,8 @@ set /p declation=输入提交的commit信息:
 git commit -m "%declation%"
 echo;
  
-echo 将变更情况提交到远程自己分支：git push origin main
-git push origin main
+echo 将变更情况提交到远程自己分支：git push origin master
+git push origin master
 echo;
  
 echo 执行完毕！
