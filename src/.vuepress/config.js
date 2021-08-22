2b631cbc8dce59f1ff907562cd6dc2f8ce27a54f
nanci <yunlong.yang@casstime.com>
2021-08-22 14:02:00 +0800

    插件配置

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -62,22 +62,23 @@ module.exports = config({
       status: "global",
     },
 
-    mdEnhance: {
-      // please only enable the features you need
-      enableAll: true,
-      presentation: {
-        plugins: [
-          "highlight",
-          "math",
-          "search",
-          "notes",
-          "zoom",
-          "anything",
-          "audio",
-          "chalkboard",
-        ],
-      },
-    },
+    // 全部开启过于消耗性能，在插件配置中进行按需引入
+    // mdEnhance: {
+    //   // please only enable the features you need
+    //   enableAll: true,
+    //   presentation: {
+    //     plugins: [
+    //       "highlight",
+    //       "math",
+    //       "search",
+    //       "notes",
+    //       "zoom",
+    //       "anything",
+    //       "audio",
+    //       "chalkboard",
+    //     ],
+    //   },
+    // },
 
     // 评论设置,结合github actions使用，避免暴露个人重要信息
     comment: {
@@ -240,51 +241,10 @@ module.exports = config({
   },
   // 插件配置
   plugins: [
-    // 樱花
-    // ["sakura", {
-    //   num: 15,  // 默认数量
-    //   show: true, //  是否显示
-    //   zIndex: 0,   // 层级
-    //   img: {
-    //     replace: false,  // false 默认图 true 换图 需要填写httpUrl地址
-    //     httpUrl: '...'     // 绝对路径
-    //   }
-    // }],
+    
     // 阅读进度条
     ['reading-progress'],
-    // 彩带背景
-    [
-      'ribbon-animation',
-      {
-        size: 60,   // 默认数据90
-        opacity: 0.2,  //  透明度
-        zIndex: -1,   //  层级
-        opt: {
-          // 色带HSL饱和度
-          colorSaturation: "80%",
-          // 色带HSL亮度量
-          colorBrightness: "60%",
-          // 带状颜色不透明度
-          colorAlpha: 0.65,
-          // 在HSL颜色空间中循环显示颜色的速度有多快
-          colorCycleSpeed: 6,
-          // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
-          verticalPosition: "center",
-          // 到达屏幕另一侧的速度有多快
-          horizontalSpeed: 200,
-          // 在任何给定时间，屏幕上会保留多少条带
-          ribbonCount: 2,
-          // 添加笔划以及色带填充颜色
-          strokeSize: 0,
-          // 通过页面滚动上的因子垂直移动色带
-          parallaxAmount: -0.5,
-          // 随着时间的推移，为每个功能区添加动画效果
-          animateSections: true
-        },
-        ribbonShow: false, //  点击彩带  true显示  false为不显示
-        ribbonAnimationShow: true  // 滑动彩带
-      }
-    ],
+    
     // 鼠标特效插件
     [
       'cursor-effects',
@@ -317,7 +277,62 @@ module.exports = config({
     ['loading-page'],
     // 集成了element-ui的插件
     ['element-ui'],
+    // markdown增强插件
+    [
+      "md-enhance",
+      {
+        // 开启标记
+        mark: true,
+        // 启用幻灯片
+        presentation: true,
+      },
+    ],
     // 其他自定义组件
-    common_plugin
+    common_plugin,
+
+    // 樱花
+    // ["sakura", {
+    //   num: 15,  // 默认数量
+    //   show: true, //  是否显示
+    //   zIndex: 0,   // 层级
+    //   img: {
+    //     replace: false,  // false 默认图 true 换图 需要填写httpUrl地址
+    //     httpUrl: '...'     // 绝对路径
+    //   }
+    // }],
+
+    // 彩带背景
+    // [
+    //   'ribbon-animation',
+    //   {
+    //     size: 60,   // 默认数据90
+    //     opacity: 0.2,  //  透明度
+    //     zIndex: -1,   //  层级
+    //     opt: {
+    //       // 色带HSL饱和度
+    //       colorSaturation: "80%",
+    //       // 色带HSL亮度量
+    //       colorBrightness: "60%",
+    //       // 带状颜色不透明度
+    //       colorAlpha: 0.65,
+    //       // 在HSL颜色空间中循环显示颜色的速度有多快
+    //       colorCycleSpeed: 6,
+    //       // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+    //       verticalPosition: "center",
+    //       // 到达屏幕另一侧的速度有多快
+    //       horizontalSpeed: 200,
+    //       // 在任何给定时间，屏幕上会保留多少条带
+    //       ribbonCount: 2,
+    //       // 添加笔划以及色带填充颜色
+    //       strokeSize: 0,
+    //       // 通过页面滚动上的因子垂直移动色带
+    //       parallaxAmount: -0.5,
+    //       // 随着时间的推移，为每个功能区添加动画效果
+    //       animateSections: true
+    //     },
+    //     ribbonShow: false, //  点击彩带  true显示  false为不显示
+    //     ribbonAnimationShow: true  // 滑动彩带
+    //   }
+    // ],
   ],
 });
